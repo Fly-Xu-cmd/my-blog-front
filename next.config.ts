44ce3b5cd25d1d75095912713e08a5b8bebcad32
Fly <1551681944@qq.com>
2026-04-19 09:22:56 +0800

    fix(config): 更新图片远程源配置，使用 HTTPS 和正确的主机名

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http", // 如果你有 https，这里写 https
-        hostname: "xylxf.xyz",
+        protocol: "https", // 如果你有 https，这里写 https
+        hostname: "image.xylxf.xyz",
         port: "", // 如果没有特殊端口就留空
         pathname: "/**", // 允许该域名下的所有路径
-      },
-      // 如果你同时用 blog.xylxf.xyz，也要加上
-      {
-        protocol: "http",
-        hostname: "blog.xylxf.xyz",
-        port: "",
-        pathname: "/**",
       },
     ],
   },
